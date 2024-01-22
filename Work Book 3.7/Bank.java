b3b0cc828b2bd5d5ab0295e692acbdd124d9cf20
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-01-21 22:08:54 -0300

    Added Bank.java

--- Work Book 3.7/Bank.java
+++ Work Book 3.7/Bank.java
@@ -6,26 +6,25 @@ public class Bank {
         Scanner scan = new Scanner(System.in);
         System.out.println("\n****ROYAL BANK OF JAVA****");
         System.out.println("Are you here to get a mortgage? (yes or no)");
-        String response = scan.nextLine();
+        String decision = scan.nextLine();
 
         //Task 2 - Print this if the decision is "yes"
-        if(response == "yes"){
+        if(decision.toLowerCase().equals("yes")){
             System.out.println("\nGreat! In one line" +
             "\nHow much money do you have in your savings?" +
             "\nAnd, how much do you owe in credit card debt?");
-                // Task 3 - Pick up each value 
             
-            scan.nextLine();
             double savings = scan.nextDouble();
             double cardDebt = scan.nextDouble();
             System.out.println("\nHow many years have you worked for?");
             int years = scan.nextInt();
 
             System.out.println("What is your name?");
-            String userName = scan.nextLine();
+            scan.nextLine();
+            String name = scan.nextLine();
 
-            if(){
-                System.out.println("Congratulations <name> You have been approved!");
+            if(savings >= 10000 && cardDebt < 5000 && years > 2){
+                System.out.println("Congratulations " + name + " You have been approved!");
             } else {
                 System.out.println("Sorry, you are not eligible for a mortgage.");
             }
