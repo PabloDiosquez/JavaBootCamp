873e0f88bf5e69bd4c3e8419591b075356bcb095
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-01 22:34:43 -0300

    Added Parameters.java

--- Section four/Chorus.java
+++ Section four/Chorus.java
@@ -1,4 +1,13 @@
+/**
+ * The Chorus class represents a program that sings a chorus five times.
+ */
 public class Chorus {
+
+    /**
+     * The main method of the Chorus class, which is the entry point of the program.
+     *
+     * @param args Command-line arguments (not used in this program).
+     */
     public static void main(String[] args) {
         // Chorus needs to be sung 5 times
         singChorus();
@@ -8,7 +17,10 @@ public class Chorus {
         singChorus();
     }
 
-    static void singChorus(){
+    /**
+     * The singChorus method prints the lyrics of the chorus to the console.
+     */
+    public static void singChorus(){
         System.out.println("Don't blame it on the sunshine");
         System.out.println("Don't blame it on the moonlight");
         System.out.println("Don't blame it on good times");
