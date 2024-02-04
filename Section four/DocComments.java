4b22a8dfd408a712b74f96d709d2e4c7bbffd281
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-03 22:39:36 -0300

    Added DocComments.java

--- Section four/DocComments.java
+++ Section four/DocComments.java
@@ -1,6 +1,6 @@
 public class DocComments {
     public static void main(String[] args) {
-        
+        greet();
     }
 
     /**
@@ -17,12 +17,26 @@ public class DocComments {
      * Function name: printText
      * 
      * @param name (String)
-     * @param age (String)
+     * @param age  (String)
      *
      * Inside the function:
      *  1. Prints the name and age as part of the text.
     */
     public static void printText(String name, String age){
         System.out.println("Hi! I'm " + name + " and I'm " + age + " years old.");
+    }
+
+    /**
+     * Function name: calculateArea
+     * 
+     * @param lenght (double)
+     * @param width  (double)
+     * @return       (double)
+     * 
+     * Inside the function:
+     *  1. calculates the area and returns it.
+     */
+    public static double calculateArea(double lenght, double width){
+        return lenght*width;
     }
 }
