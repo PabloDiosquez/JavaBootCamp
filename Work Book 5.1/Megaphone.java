b8b92a041e9378837ba6db2a8a8bdc4604b8d315
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-05 23:21:14 -0300

    Added Megaphone.java

--- Work Book 5.1/Megaphone.java
+++ Work Book 5.1/Megaphone.java
@@ -1,9 +1,17 @@
+/**
+ * The Megaphone class demonstrates a simple Java program that prints a message multiple times using a for loop.
+ */
 public class Megaphone {
+    /**
+     * The main method is the entry point of the program.
+     *
+     * @param args The command-line arguments (not used in this example).
+     */
     public static void main(String[] args) {
-        
+        // Using a for loop to repeat the printing of a message 10 times
         for (int i = 0; i < 10; i++) {
-            System.out.println(i + ". If Java was easy, the would call it Python!");
+            // Printing a message with the current loop index (i) and a fixed text
+            System.out.println(i + ". If Java was easy, they would call it Python!");
         }
     }
-    
 }
