2e7dfe6ad141a118e6ebf31a1d5a043b7c0ce04a
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-06 14:28:27 -0300

    Added EvenOrOdd.java

--- Work Book 5.6/EvenOrOdd.java
+++ Work Book 5.6/EvenOrOdd.java
@@ -1,4 +1,15 @@
+/**
+ * The EvenOrOdd class determines whether numbers from 0 to 19 are even or odd.
+ * It iterates through the numbers and prints whether each number is even or odd.
+ */
 public class EvenOrOdd {
+    
+    /**
+     * The main method of the EvenOrOdd class.
+     * It iterates through numbers from 0 to 19 and prints whether each number is even or odd.
+     *
+     * @param args The command-line arguments passed to the program (unused).
+     */
     public static void main(String[] args) {
         for (int i = 0; i < 20; i++) {
             if(isEven(i)){
@@ -9,6 +20,13 @@ public class EvenOrOdd {
         }
     }
 
+    /**
+     * Determines whether a number is even or odd.
+     *
+     * @param number The number to check.
+     *               It can be any integer value.
+     * @return true if the number is even, false otherwise.
+     */
     public static boolean isEven(int number){
         return number % 2 == 0;
     }
