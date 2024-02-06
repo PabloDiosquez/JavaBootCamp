484b8b140bc28eacb54de12b50035fb51dc524a7
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-05 23:07:04 -0300

    Added ForLoopExamples.java

--- Section five/ForLoopExamples.java
+++ Section five/ForLoopExamples.java
@@ -1,9 +1,17 @@
+/**
+ * This class provides examples of for loops in Java.
+ */
 public class ForLoopExamples {
 
+    /**
+     * Main method containing examples of for loops.
+     *
+     * @param args The command line arguments (not used in this example).
+     */
     public static void main(String[] args) {
         // Example 1: Print numbers from 1 to 10
         // TODO: Write a for loop that prints numbers from 1 to 10
-        for(int i = 1; i <= 10; i++){
+        for(int i = 1; i < 11; i++){
             System.out.println(i);
         }
 
@@ -13,13 +21,11 @@ public class ForLoopExamples {
             System.out.println("Hello World!");
         }
 
-
         // Example 3: Print the multiplication table of 5 up to 10
         // TODO: Write a for loop that prints the multiplication table of 5 up to 10 (5 x 1, 5 x 2, ..., 5 x 10)
-        for (int i = 0; i <= 10; i++) {
-            System.out.println("5 x " + i + " = " + 5*i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("5 x " + i + " = " + 5 * i);
         }
-
 
         // Example 4: Print the numbers from 10 to 1 in reverse order
         // TODO: Write a for loop that prints numbers from 10 to 1 in reverse order
@@ -29,7 +35,7 @@ public class ForLoopExamples {
 
         // Example 5: Print the first 10 even numbers
         // TODO: Write a for loop that prints the first 10 even numbers
-        for (int i = 0; i < 21; i+=2) {
+        for (int i = 0; i < 20; i += 2) {
             System.out.println(i);
         }
     }
