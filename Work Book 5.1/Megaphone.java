577c39df6c689070d1fa2a8562da247f3aeb1b1c
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-05 23:20:29 -0300

    Added Megaphone.java

---
+++ Work Book 5.1/Megaphone.java
@@ -0,0 +1,9 @@
+public class Megaphone {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ". If Java was easy, the would call it Python!");
+        }
+    }
+    
+}
