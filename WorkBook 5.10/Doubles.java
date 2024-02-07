60dda4cc473aa000e3bb0db0d8990bc246ebf659
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-07 13:39:10 -0300

    Added Doubles.java

---
+++ WorkBook 5.10/Doubles.java
@@ -0,0 +1,27 @@
+public class Doubles {
+    public static void main(String[] args) {
+        int dice1, dice2;
+        dice1 = rollDice();
+        dice2 = rollDice();
+
+        System.out.print("Dice 1: " + dice1);
+        System.out.print("\nDice 2: " + dice2);
+
+
+    }
+
+    /**
+     * Function name: rollDice -- rolls a dice between 1 and 6
+     * 
+     * @return randomNumber (int)
+     */
+    public static int rollDice(){
+        return (int)(Math.random()*6 + 1);
+    }
+
+}
+
+
+
+
+
