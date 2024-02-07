a2493a8c2953ea111f70336a3e2c970e4c75291c
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-07 15:12:54 -0300

    Added JavaRolls.java

--- WorkBook 5.11/JavaRolls.java
+++ WorkBook 5.11/JavaRolls.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
 
+/**
+ * The JavaRolls class implements a simple text-based dice rolling game called Rolling Java.
+ * Players roll a virtual dice, aiming to score points and win the game by reaching a certain score.
+ */
 public class JavaRolls {
+
+    /**
+     * The main method starts the Rolling Java game.
+     * 
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
         System.out.println("Let's play Rolling Java. Type anything to start.");
         scan.nextLine();
 
-        System.out.println("Great! Here are the rules:\n>> If you roll a 6, the game stops.\n>>If you roll a 4, nothing happens.\n>>Otherwise, you get 1 point.\n>>You must collect at least 3 points to win. Enter anything to roll:");
+        System.out.println("Great! Here are the rules:\n>> If you roll a 6, the game stops.\n>> If you roll a 4, nothing happens.\n>> Otherwise, you get 1 point.\n>> You must collect at least 3 points to win. Enter anything to roll:");
         scan.nextLine();
         
         int diceRoll;
@@ -32,24 +42,14 @@ public class JavaRolls {
         } else {
             System.out.println("Tough luck, you lose :(");
         }
-
-
     }
 
     /**
-     * Function name: rollDice
+     * Generates a random number between 1 and 6, simulating a dice roll.
      * 
-     * @return randomNumber (int)
-     * 
-     * Inside the function: 
-     *  - return a random number between 1 and 6.
+     * @return the randomly generated number (int) representing the result of the dice roll
      */
     public static int rollDice(){
         return (int)(Math.random()*6 + 1);
     }
-  
-  
-
-  
-  
 }
