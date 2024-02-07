de9e14a94dab232bd771b9ac394c483214ec9027
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-06 22:10:40 -0300

    Added Guess.java

--- WorkBook 5.8/Guess.java
+++ WorkBook 5.8/Guess.java
@@ -1,19 +1,34 @@
-import java.util.Scanner;
+/**
+ * This class represents a simple guessing game where the user tries to guess a secret number.
+ */
+public class Guess {
 
-public class Guess  {
+    /**
+     * The main method of the Guess class.
+     * This method initializes a secret number and prompts the user to guess it.
+     * It continues to prompt the user until the correct guess is made.
+     * @param args The command-line arguments passed to the program (not used in this program).
+     */
     public static void main(String[] args) {
-       int secretNumber = 5;
+        // The secret number to be guessed
+        int secretNumber = 5;
 
-       System.out.print("I chose a number between 1 and 5. Try to guess it: ");
-       
-       Scanner scan = new Scanner(System.in);
+        // Prompt the user to guess a number between 1 and 5
+        System.out.print("I chose a number between 1 and 5. Try to guess it: ");
 
-       while (scan.nextInt() != secretNumber) {
+        // Initialize a scanner object to read user input
+        Scanner scan = new Scanner(System.in);
+
+        // Continuously prompt the user to guess until they guess the correct number
+        while (scan.nextInt() != secretNumber) {
+            // If the guess is incorrect, prompt the user to guess again
             System.out.print("Guess again: ");
-       }
-       System.out.println("You got it!");
-    
+        }
+
+        // If the correct guess is made, inform the user
+        System.out.println("You got it!");
+
+        // Close the scanner to release system resources
         scan.close();
     }
-
 }
