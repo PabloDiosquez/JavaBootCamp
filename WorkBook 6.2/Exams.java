1760c535c84920ac52daf88549089bf79fc6f3db
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-09 15:38:51 -0300

    Added ArrayLoopExamples.java

---
+++ WorkBook 6.2/Exams.java
@@ -0,0 +1,13 @@
+public class Exams {
+    public static void main(String[] args) {
+        
+        // Instructions for this workbook are on Learn the Part (Workbook 6.2).
+        System.out.println("It's time to take your 5th years exams. Please, take your seats.\n");
+
+        String[] seats = {"Malfoy", "Crabbe", "Goyle", "Pansy", "Dean"};
+        
+        for (int i = 0; i < seats.length; i++) {
+            System.out.println(seats[i] + ", you will take seat " + i);
+        }
+    }
+}
