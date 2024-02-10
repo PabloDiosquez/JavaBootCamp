07dce0d99bc014940d0c548de6560b2346ebbc93
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-10 11:09:15 -0300

    Added Grocer.java

--- WorkBook 6.3/Grocer.java
+++ WorkBook 6.3/Grocer.java
@@ -1,11 +1,25 @@
+/**
+ * This class represents a grocer with a list of aisles containing different products.
+ */
 public class Grocer {
+
+    /**
+     * The main method prints out the aisle number where coffee is located.
+     * 
+     * @param args Command-line arguments (not used).
+     */
     public static void main(String[] args) {
+        // Array representing the aisles in the store
         String[] aisles = {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
 
+        // Print a message asking if coffee is sold
         System.out.println("\nDo you sell coffee?");
 
+        // Iterate through the aisles to find coffee
         for (int i = 0; i < aisles.length; i++) {
+            // Check if the current aisle contains coffee
             if(aisles[i].equals("coffee")){
+                // Print the aisle number where coffee is located
                 System.out.println("\nWe have that in aisle: " + i);
             }
         }
