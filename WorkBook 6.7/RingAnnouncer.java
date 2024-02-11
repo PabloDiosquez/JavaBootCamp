bc99e06a9b5dbd90538cb651b8736abe5be53d82
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-11 10:38:13 -0300

    Added RingAnnouncer.java

--- WorkBook 6.7/RingAnnouncer.java
+++ WorkBook 6.7/RingAnnouncer.java
@@ -1,9 +1,20 @@
+/**
+ * The RingAnnouncer class keeps track of wins and losses in a boxing match.
+ */
 public class RingAnnouncer {
+    
+    /**
+     * The main method counts the number of wins and losses in a boxing match record.
+     * 
+     * @param args The command-line arguments (unused).
+     */
     public static void main(String[] args) {
+        // Array representing the boxing match record
         String[] record = {"WIN", "WIN", "WIN", "LOSS", "WIN", "WIN", "LOSS"};
         int wins = 0;
         int losses = 0;
 
+        // Count the number of wins and losses
         for (String result : record) {
             if(result.equals("WIN")){
                 wins++;
@@ -12,7 +23,8 @@ public class RingAnnouncer {
             } 
         }
         
+        // Display the professional record
         System.out.println("\nWith a professional record of " + wins + " Wins and " + losses + " losses.");
-        System.out.println("He is the pride of oracle: Java Fury!");
+        System.out.println("He is the pride of Oracle: Java Fury!");
     }
 }
