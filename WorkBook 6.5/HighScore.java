7f2c69453765fc701a64017f0fe5970bdcf0872c
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-11 10:21:53 -0300

    Added HighScore.java

--- WorkBook 6.5/HighScore.java
+++ WorkBook 6.5/HighScore.java
@@ -1,5 +1,15 @@
+/**
+ * The HighScore class generates an array of random scores and identifies the highest score.
+ */
 public class HighScore {
+    
+    /**
+     * The main method generates an array of random scores and identifies the highest score.
+     * 
+     * @param args The command-line arguments (unused).
+     */
     public static void main(String[] args) {
+        // Generate an array of random scores
         int[] scores = {
             randomNumber(),
             randomNumber(),
@@ -19,21 +29,20 @@ public class HighScore {
         for (int score : scores) {
             System.out.print(score + " ");
 
+            // Update highScore if the current score is higher
             if(score > highScore){
                 highScore = score;
             }
         }
 
+        // Display the highest score
         System.out.println("\n\nThe highest score is: " + highScore + ". Give that man a cookie!");
     } 
     
     /**
-     * Function name: randomNumber
+     * Generates a random number between 0 and 499999 (inclusive).
      * 
-     * @return (int)
-     * 
-     * Inside the function:
-     *  - return a random number between 0 and 499999.
+     * @return The generated random number.
      */
     public static int randomNumber(){
         return (int)(Math.random()*500000);
