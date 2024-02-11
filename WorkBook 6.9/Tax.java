c06c1b9da8b9bbbc96ec32742556f0a6705d89da
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-11 13:54:23 -0300

    Added Tax.java

--- WorkBook 6.9/Tax.java
+++ WorkBook 6.9/Tax.java
@@ -1,16 +1,30 @@
 import java.util.Arrays;
 
+/**
+ * This class calculates the price after applying tax.
+ */
 public class Tax {
+    
+    /**
+     * The main method of the Tax class, which calculates the price after applying tax.
+     * @param args command-line arguments (not used in this program)
+     */
     public static void main(String[] args) {
-        double[] price = {1.99, 2.99, 3.99, 4.99};
+        // Array representing original prices
+        double[] price = {1.99, 2.65, 3.99, 4.99};
 
-        double[] afterTax = new double[4];
+        // Array to store prices after tax
+        double[] afterTax = new double[price.length];
 
+        // Calculating prices after tax
         for (int i = 0; i < price.length; i++) {
-            afterTax[i] = price[i] + 0.13*price[i];
+            afterTax[i] = price[i] + 0.13 * price[i]; // Applying 13% tax
         }
 
-        System.out.print("The original price are: " + Arrays.toString(price));
-        System.out.println("The price after tax are: " + Arrays.toString(afterTax));
+        // Displaying original prices
+        System.out.println("The original prices are: " + Arrays.toString(price));
+
+        // Displaying prices after tax
+        System.out.println("The prices after tax are: " + Arrays.toString(afterTax));
     }
 }
