0efa3c405bfffce56d264c718e5c1caee0634112
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-11 10:12:38 -0300

    Added HighScore.java

--- WorkBook 6.5/HighScore.java
+++ WorkBook 6.5/HighScore.java
@@ -1,14 +1,42 @@
 public class HighScore {
     public static void main(String[] args) {
-        
+        int[] scores = {
+            randomNumber(),
+            randomNumber(),
+            randomNumber(),
+            randomNumber(),
+            randomNumber(),
+            randomNumber(),
+            randomNumber(),
+            randomNumber(),
+            randomNumber(),
+            randomNumber(),
+        };
+
         int highScore = 0;
         
-        // Instructions for this workbook are on Learn the Part (Workbook 6.5).
+        System.out.print("Here are the scores: ");
+        for (int score : scores) {
+            System.out.print(score + " ");
 
-        System.out.print("Here are the scores: <score elements>");
+            if(score > highScore){
+                highScore = score;
+            }
+        }
 
         System.out.println("\n\nThe highest score is: " + highScore + ". Give that man a cookie!");
-        
-    }    
+    } 
+    
+    /**
+     * Function name: randomNumber
+     * 
+     * @return (int)
+     * 
+     * Inside the function:
+     *  - return a random number between 0 and 499999.
+     */
+    public static int randomNumber(){
+        return (int)(Math.random()*500000);
+    }
 
 }
