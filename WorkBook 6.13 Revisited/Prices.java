0ffe95bfd2c2303590ce51fc3901263e57ae6c82
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-12 15:33:58 -0300

    Added Prices.java (Revisited)

--- WorkBook 6.13 Revisited/Prices.java
+++ WorkBook 6.13 Revisited/Prices.java
@@ -4,16 +4,19 @@ public class Prices {
             {12.99, 8.99, 9.99, 10.49, 11.99},
             {0.99, 1.99, 2.49, 1.49, 2.99},
             {8.99, 7.99, 9.49, 9.99, 10.99}};
-
-        for (int i = 0; i < args.length; i++) {
+        
+        System.out.print("Prices:\n ");
+        for (int i = 0; i < prices.length; i++) {
             switch (i) {
-                case 0: System.out.print("Baking: "); break;
-                case 1: System.out.print("Beverage: "); break;
-                case 2: System.out.print("Cereals: "); break;
+                case 0: System.out.print("\tBaking: "); break;
+                case 1: System.out.print("\tBeverage: "); break;
+                case 2: System.out.print("\tCereals: "); break;
             }
-            for (int j = 0; j < args.length; j++) {
-                
+
+            for (int j = 0; j < prices[i].length; j++) {
+                System.out.print(prices[i][j] + " ");
             }
+            System.out.print("\n");
         }
     }
 }
