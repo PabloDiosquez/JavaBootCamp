b9694e191b6fe09f613ae77825069877426d1c7a
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-16 16:12:38 -0300

    Added Car.java

---
+++ Module 2 - OOP/Section seven/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args) {
+        Car toyota = new Car();
+        toyota.make = "Toyota";
+        toyota.price = 10000;
+        toyota.year = 2020;
+        toyota.color = "Green";
+
+        Car nissan = new Car();
+        nissan.make = "Nissan";
+        nissan.price = 9000;
+        nissan.year = 2019;
+        nissan.color = "Black";
+    }
+} 
