fc67281b7b387a5803bd12081dd1588c11211d7d
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-16 16:53:15 -0300

    Added Workbook 7.1 - Main.java

--- Module 2 - OOP/WorkBook 7.1/Main.java
+++ Module 2 - OOP/WorkBook 7.1/Main.java
@@ -1,18 +1,22 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
+/**
+ * Represents a person with basic information such as name, nationality, date of birth, passport, and seat number.
+ */
 public class Main {
     public static void main(String[] args) {
         Person person = new Person();
 
         // Update each field in the object before printing them.
 
-        person.name = "Paul McCartney";
-        person.nationality = "UK";
-        person.dateOfBirth = "June 18, 1942";
-        person.passport = new String[]{"@ABC", person.name, person.nationality, person.dateOfBirth};
-        person.seatNumber = 10;
+        person.name = "Paul McCartney"; // Setting the name of the person
+        person.nationality = "UK"; // Setting the nationality of the person
+        person.dateOfBirth = "June 18, 1942"; // Setting the date of birth of the person
+        person.passport = new String[]{"@ABC", person.name, person.nationality, person.dateOfBirth}; // Creating passport information for the person
+        person.seatNumber = 10; // Setting the seat number assigned to the person
 
+        // Printing out the details of the person
         System.out.println(person.name);
         System.out.println(person.nationality);
         System.out.println(person.dateOfBirth);
