eb90c55a6aeb7f1b3c6d2d1988b553b9e7047833
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-16 16:20:45 -0300

    Added Car.java

--- Module 2 - OOP/Section seven/Main.java
+++ Module 2 - OOP/Section seven/Main.java
@@ -11,5 +11,10 @@ public class Main{
         nissan.price = 9000;
         nissan.year = 2019;
         nissan.color = "Black";
+        
+        System.out.println("This " + toyota.make + " is worth $" + toyota.price + 
+        ". It was built in " + toyota.year + ". It is " + toyota.color + ".\n");
+        System.out.println("This " + nissan.make + " is worth $" + nissan.price + 
+        ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
     }
 } 
